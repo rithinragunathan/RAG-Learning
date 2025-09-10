c5d26161d74c0c921270e22ae8761675e29eaca9
rithinragunathan <rithinragunathan@gmail.com>
2025-09-10 20:35:14 +0530

    added gemini

---
+++ main.py
@@ -0,0 +1,11 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("models/gemini-1.5-flash-latest")
+
+resp = model.generate_content("What is the capital of italy?")
+print(resp.text)
