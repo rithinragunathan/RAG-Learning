29c6289a3b8e3d2839c44e6597b4e3fbdccffc4e
rithinragunathan <rithinragunathan@gmail.com>
2025-09-11 15:47:10 +0530

    Intial protype

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import chromadb
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
+emb_model = "models/text-embedding-004"
 model = genai.GenerativeModel("models/gemini-1.5-flash-latest")
 
-resp = model.generate_content("What is the capital of italy?")
-print(resp.text)
+db_client = chromadb.PersistentClient("VectorDB")
+
+collections = db_client.get_or_create_collection(
+    name="Embeddings",
+    configuration=emb_model)
+
